77391fedd4e0cf864b2b14da14a19c267f1916b8
Sabyrzhan <144157934+Sabyrzhan-Y@users.noreply.github.com>
2023-12-25 23:46:18 +0500

    Задание 2 выполнено!

--- HW_6-2-2.c
+++ HW_6-2-2.c
@@ -3,9 +3,12 @@
 int main(void)
 {
 	int a, b, c;
-	int sum;
+	int sum, S;
 	scanf("%d%d%d", &a, &b, &c);
 	sum = a + b + c;
+	S = a * b * c;
+    
 	printf("%d + %d + %d = %d\n", a, b, c, sum);
+	printf("%d * %d * %d = %d\n", a, b, c, S);
 	return 0;
 }
